ceb033763c4ef4358fdab4412edcbd03705ce516
Warren James <warren.james@mongodb.com>
2024-01-17 15:50:08 -0500

    Unskip tests and pull in updated test

--- test/integration/server-discovery-and-monitoring/server_discovery_and_monitoring.spec.test.ts
+++ test/integration/server-discovery-and-monitoring/server_discovery_and_monitoring.spec.test.ts
@@ -5,10 +5,5 @@ import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 
 describe('SDAM Unified Tests (Spec)', function () {
   const specTests = loadSpecTests(path.join('server-discovery-and-monitoring', 'unified'));
-  runUnifiedSuite(specTests, test => {
-    if (['Topology lifecycle'].includes(test.description)) {
-      return 'see NODE-5723';
-    }
-    return false;
-  });
+  runUnifiedSuite(specTests);
 });
